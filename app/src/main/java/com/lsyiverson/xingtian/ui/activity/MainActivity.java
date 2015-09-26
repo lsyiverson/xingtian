6e9bebae9ec7fdc4947a4801a48a4ab68e89d78a
lsyiverson <lsyiverson@gmail.com>
2015-09-27 00:18:45 +0800

    [lsyiverson] - config project

--- app/src/main/java/com/lsyiverson/xingtian/ui/activity/MainActivity.java
+++ app/src/main/java/com/lsyiverson/xingtian/ui/activity/MainActivity.java
@@ -1,10 +1,12 @@
-package com.lsyiverson.xingtian;
+package com.lsyiverson.xingtian.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import com.lsyiverson.xingtian.R;
 
 public class MainActivity extends AppCompatActivity {
 
