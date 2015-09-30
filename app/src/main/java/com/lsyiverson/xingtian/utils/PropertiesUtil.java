5872f13d7be16d3f4a62b2b267d3998d9026bd4b
lsyiverson <lsyiverson@gmail.com>
2015-09-30 22:40:21 +0800

    [lsyiverson]: use properties file to customize property in different environment

--- app/src/main/java/com/lsyiverson/xingtian/utils/PropertiesUtil.java
+++ app/src/main/java/com/lsyiverson/xingtian/utils/PropertiesUtil.java
@@ -2,25 +2,64 @@ package com.lsyiverson.xingtian.utils;
 
 import android.content.Context;
 
-import com.github.fernandodev.androidproperties.lib.AssetsProperties;
+import org.apache.commons.configuration.CompositeConfiguration;
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
 
-public class PropertiesUtil extends AssetsProperties {
+import java.io.IOException;
+import java.util.List;
 
-    private static PropertiesUtil instance;
+import java8.util.stream.StreamSupport;
 
-    private PropertiesUtil(Context context) {
-        super(context);
-    }
+import static java8.util.stream.Collectors.toList;
+
+public class PropertiesUtil {
+    private static final String ENV_CONFIG_PROPERTIES = "env_config.properties";
+    private static final String BASE_CONFIG_PROPERTIES = "base_config.properties";
+
+    private static Configuration configuration;
 
     public static void init(Context context) {
-        instance = new PropertiesUtil(context);
+        try {
+            String[] list = context.getAssets().list("");
+
+            List<String> properties = StreamSupport.of(list)
+                .filter(s -> s.endsWith(".properties"))
+                .collect(toList());
+
+            CompositeConfiguration compositeConfiguration = new CompositeConfiguration();
+
+            // priority 1: if env_config.properties is existed
+            // load env_config.properties defined in environment module.
+            if (properties.contains(ENV_CONFIG_PROPERTIES)) {
+                PropertiesConfiguration envConfig = new PropertiesConfiguration();
+                envConfig.load(context.getAssets().open(ENV_CONFIG_PROPERTIES));
+                compositeConfiguration.addConfiguration(envConfig);
+            }
+
+            // priority 2: load default base_config.properties
+            PropertiesConfiguration baseConfig = new PropertiesConfiguration();
+            baseConfig.load(context.getAssets().open(BASE_CONFIG_PROPERTIES));
+            compositeConfiguration.addConfiguration(baseConfig);
+
+            configuration = compositeConfiguration;
+        } catch (ConfigurationException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static String getString(String key) {
-        return instance.getString(key, "");
+        return configuration.getString(key);
     }
 
     public static int getInt(String key) {
-        return instance.getInt(key, 0);
+        return configuration.getInt(key);
+    }
+
+    public static boolean getBoolean(String key) {
+        return configuration.getBoolean(key);
     }
 }
