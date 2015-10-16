7cdbbac76370f2abd376fca5f99df03e46c9c5a6
lsyiverson <lsyiverson@gmail.com>
2015-10-16 23:04:45 +0800

    [lsyiverson]: just set one listener every time

--- app/src/main/java/com/lsyiverson/xingtian/receiver/PhoneStatReceiver.java
+++ app/src/main/java/com/lsyiverson/xingtian/receiver/PhoneStatReceiver.java
@@ -5,43 +5,56 @@ import android.content.Context;
 import android.content.Intent;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
-import android.util.Log;
 
 import com.lsyiverson.xingtian.service.MobileInfoService;
 
+import java.util.concurrent.TimeUnit;
+
+import rx.Observable;
+import rx.Subscriber;
+
 public class PhoneStatReceiver extends BroadcastReceiver {
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.d("TAG", "broadcast");
-        if (intent.getAction() == TelephonyManager.ACTION_PHONE_STATE_CHANGED) {
-            TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-            telephonyManager.listen(new Listener(context), PhoneStateListener.LISTEN_CALL_STATE);
-        }
+        TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+
+        Observable.just(intent.getAction())
+            .filter(TelephonyManager.ACTION_PHONE_STATE_CHANGED::equals)
+            .filter(action -> telephonyManager.getCallState() == TelephonyManager.CALL_STATE_RINGING)
+            .flatMap(action -> Observable.create(new PhoneStateOnSubscribe(context, telephonyManager)))
+            .debounce(200, TimeUnit.MILLISECONDS)
+            .subscribe(serviceIntent -> context.startService(serviceIntent));
     }
 
-    private static class Listener extends PhoneStateListener {
+    private final class PhoneStateOnSubscribe implements Observable.OnSubscribe<Intent> {
         private final Context context;
+        private final TelephonyManager telephonyManager;
 
-        public Listener(Context context) {
+        PhoneStateOnSubscribe(Context context, TelephonyManager telephonyManager) {
             this.context = context;
+            this.telephonyManager = telephonyManager;
         }
 
         @Override
-        public void onCallStateChanged(int state, String incomingNumber) {
-            super.onCallStateChanged(state, incomingNumber);
+        public void call(Subscriber<? super Intent> subscriber) {
+            PhoneStateListener listener = new PhoneStateListener(){
+                @Override
+                public void onCallStateChanged(int state, String incomingNumber) {
+                    super.onCallStateChanged(state, incomingNumber);
 
-            switch (state) {
-                case TelephonyManager.CALL_STATE_IDLE:
-                    break;
-                case TelephonyManager.CALL_STATE_OFFHOOK:
-                    break;
-                case TelephonyManager.CALL_STATE_RINGING:
-                    Intent intent = new Intent(context, MobileInfoService.class);
-                    intent.putExtra(MobileInfoService.MOBILE, incomingNumber);
-                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    context.startService(intent);
-                    break;
-            }
+                    switch (state) {
+                        case TelephonyManager.CALL_STATE_RINGING:
+                            Intent intent = new Intent(context, MobileInfoService.class);
+                            intent.putExtra(MobileInfoService.MOBILE, incomingNumber);
+                            subscriber.onNext(intent);
+                            break;
+                    }
+                }
+            };
+
+            telephonyManager.listen(listener, PhoneStateListener.LISTEN_CALL_STATE);
+            telephonyManager.listen(listener, PhoneStateListener.LISTEN_NONE);
         }
     }
 }
